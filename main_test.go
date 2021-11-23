cd5ad1bc2c8f4e66709fad10954a58e73760fb63
Matt Rutkowski <mrutkows@us.ibm.com>
2021-11-23 17:49:42 -0600

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -7,12 +7,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TestHelloName calls greetings.Hello with a name, checking
-// for a valid return value.
+// Test simple 1 reader referencing 1 version
 func TestSimple(t *testing.T) {
 
 	versions := []int{1, 5, 10, 14}
+
+	// readers 11, 12 should both reference version 10
 	readers := []int{7, 11}
+	expected := []int{5, 10, 14}
+
+	result, err := Gc2(versions, readers)
+
+	if err != nil {
+		fmt.Printf("error=%v\n", err)
+	} else {
+		fmt.Printf("expected=%v; result=%v\n", expected, result)
+	}
+
+	assert.True(t, true, "True is true!")
+	assert.EqualValues(t, expected, result, "expected!=result")
+
+}
+
+// Test 2 readers referencing same version
+// It also, tests this condition at the end of the versions vector
+func TestSimple2(t *testing.T) {
+
+	versions := []int{1, 5, 10, 14}
+
+	// readers 11, 12 should both reference version 10
+	readers := []int{7, 11, 12}
 	expected := []int{5, 10, 14}
 
 	result, err := Gc2(versions, readers)
